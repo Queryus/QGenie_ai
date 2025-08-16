ff6e4377b6fe0da0ea6f65d336ffe24287e1df27
CheckerBoard <hyeong8465@naver.com>
2025-08-16 23:02:41 +0900

    fix: 포트번호 고정

--- src/main.py
+++ src/main.py
@@ -6,12 +6,12 @@ import uvicorn
 from fastapi import FastAPI
 from api.v1.endpoints import chat, annotator
 
-def find_free_port():
-    """사용 가능한 비어있는 포트를 찾는 함수"""
-    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
-        s.bind(('', 0))
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        return s.getsockname()[1]
+# def find_free_port():
+#     """사용 가능한 비어있는 포트를 찾는 함수"""
+#     with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
+#         s.bind(('', 0))
+#         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+#         return s.getsockname()[1]
 
 # FastAPI 앱 인스턴스 생성
 app = FastAPI(
@@ -41,7 +41,8 @@ def health_check():
 
 if __name__ == "__main__":
     # 1. 비어있는 포트 동적 할당
-    free_port = find_free_port()
+    # free_port = find_free_port()
+    free_port = 35816 # 포트 번호 고정
 
     # 2. 할당된 포트 번호를 콘솔에 특정 형식으로 출력
     print(f"PYTHON_SERVER_PORT:{free_port}")
